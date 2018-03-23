5d329cc072370c2d177c966442ddf87d3eb02565
Marina Tzenkova <m.tsenkova@student.fontys.nl>
2018-03-23 19:50:20 +0100

    Fixing permissions on map and other activities

--- app/src/main/java/com/example/marinac/riddletheflag/MapActivity.java
+++ app/src/main/java/com/example/marinac/riddletheflag/MapActivity.java
@@ -1,5 +1,7 @@
 package com.example.marinac.riddletheflag;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -7,10 +9,13 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.provider.MediaStore;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,6 +31,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private LocationManager locationManager;
+    private static final int MY_PERMISSIONS_REQUEST_ACESS_FINE_LOCATION = 2;
+    private static final int MY_PERMISSIONS_REQUEST_ACESS_COARSE_LOCATION = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +44,15 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MapActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACESS_FINE_LOCATION);
+            }
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MapActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACESS_COARSE_LOCATION);
             }
             if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
@@ -125,15 +132,29 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACESS_COARSE_LOCATION:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+
+                    Toast.makeText(this, "Permission was denied!",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case MY_PERMISSIONS_REQUEST_ACESS_FINE_LOCATION:
+                if(grantResults.length>0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                }else {
+                    Toast.makeText(this, "Permission was denied!", Toast.LENGTH_SHORT).show();
+                }
+
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
--- app/src/main/java/com/example/marinac/riddletheflag/RegisterAcitvity.java
+++ app/src/main/java/com/example/marinac/riddletheflag/RegisterAcitvity.java
@@ -1,12 +1,23 @@
 package com.example.marinac.riddletheflag;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -15,34 +26,51 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
 public class RegisterAcitvity extends AppCompatActivity implements View.OnClickListener{
 
     ProgressBar progressBar;
-    EditText nameTB, passTB;
+    private EditText emailTb, passTB, nameTb;
+    private Drawable d;
     private FirebaseAuth mAuth;
+    private StorageReference mStorageRef;
+    private static final int SELECTED_PICTURE = 1;
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 2;
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_acitvity);
-        mAuth = FirebaseAuth.getInstance();
 
-        nameTB = findViewById(R.id.nameTB);
+        mAuth = FirebaseAuth.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        emailTb = findViewById(R.id.emailTb);
         passTB = findViewById(R.id.passTB);
+        nameTb = findViewById(R.id.NameTb);
         progressBar = findViewById(R.id.progressBar);
+        iv = (ImageView)findViewById(R.id.profileImageView);
 
         findViewById(R.id.registerBtn).setOnClickListener(this);
+        findViewById(R.id.uploadPictureBtn).setOnClickListener(this);
+
+        emailTb.setOnClickListener(this);
+        passTB.setOnClickListener(this);
+        nameTb.setOnClickListener(this);
     }
 
     private void RegisterUser(){
-        String email = nameTB.getText().toString().trim();
+        String email = emailTb.getText().toString().trim();
         String pass = passTB.getText().toString().trim();
+        String name = nameTb.getText().toString().trim();
 
         if(email.isEmpty()){
-            nameTB.setError("Email is required!");
-            nameTB.requestFocus();
+            emailTb.setError("Email is required!");
+            emailTb.requestFocus();
             return;
         }
         if(pass.isEmpty())
@@ -51,10 +79,15 @@ public class RegisterAcitvity extends AppCompatActivity implements View.OnClickL
             passTB.requestFocus();
             return;
         }
+        if(name.isEmpty())
+        {
+            nameTb.setError("Name is required!");
+            nameTb.requestFocus();
+        }
 
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            nameTB.setError("Please enter a valid email!");
-            nameTB.requestFocus();
+            emailTb.setError("Please enter a valid email!");
+            emailTb.requestFocus();
             return;
         }
         if(pass.length()<6){
@@ -85,7 +118,9 @@ public class RegisterAcitvity extends AppCompatActivity implements View.OnClickL
                 }
             }
         });
+        progressBar.setVisibility(View.GONE);
     }
+
 
     @Override
     public void onClick(View view) {
@@ -93,7 +128,84 @@ public class RegisterAcitvity extends AppCompatActivity implements View.OnClickL
          {
              case R.id.registerBtn:
                  RegisterUser();
-             break;
+                break;
+
+             case R.id.emailTb:
+                 emailTb.getText().clear();
+                 break;
+
+             case R.id.passTB:
+                 passTB.getText().clear();
+                 break;
+
+             case R.id.NameTb:
+                 nameTb.getText().clear();
+                 break;
+
+             case R.id.uploadPictureBtn:
+                 if (ContextCompat.checkSelfPermission(RegisterAcitvity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                         != PackageManager.PERMISSION_GRANTED) {
+                     // Permission is not granted
+                     ActivityCompat.requestPermissions(RegisterAcitvity.this,
+                             new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                             MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+
+
+                 } else {
+                     Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                     startActivityForResult(i, SELECTED_PICTURE);
+                 }
+                 break;
          }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(i, SELECTED_PICTURE);
+                } else {
+
+                    Toast.makeText(this, "Permission was denied!",Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode)
+        {
+            case SELECTED_PICTURE:
+                if(resultCode==RESULT_OK){
+                    Uri uri = data.getData();
+                    String[]projection = {MediaStore.Images.Media.DATA};
+
+                    Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(projection[0]);
+                    String filepath = cursor.getString(columnIndex);
+
+                    Bitmap yourSelectedImage = BitmapFactory.decodeFile(filepath);
+                    d = new BitmapDrawable(yourSelectedImage);
+
+                    iv.setBackground(d);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Something went wrong!",Toast.LENGTH_SHORT);
+                }
+                break;
+        }
     }
 }
